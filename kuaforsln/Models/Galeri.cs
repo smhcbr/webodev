b8c486779523c34c93d01b58407fe89f962e90b8
oguzhankirikkk <osman.oguzhan544@gmail.com>
2024-12-23 20:23:38 +0300

    Galeri.cs eklendi

---
+++ kuaforsln/Models/Galeri.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace kuaforsln.Models.Entities
+{
+    public partial class Galeri
+    {
+        [Key]
+        public int Id { get; set; }
+        public string ImgText { get; set; }
+        [Required]
+        public string ImgPath { get; set; }
+        public DateTime ImgDate { get; set; }
+        public int ImgUserId { get; set; }
+
+        public virtual User ImgUser { get; set; }
+    }
+}
